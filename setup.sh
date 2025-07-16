cda8bf1f7dfbf1bb577869ed28c0131e852e7c9e
Junnyjun <chbe5082@naver.com>
2025-07-16 11:26:54 +0900

    init

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git filter-branch --env-filter '
+WRONG_EMAIL="changhee2@finda.co.kr"
+NEW_NAME="Junnyjun"
+NEW_EMAIL="chbe5082@naver.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
