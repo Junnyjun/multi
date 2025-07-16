cb8dfac5c6dfd8fbf11a8492856a5186480c425b
Junnyjun <chbe5082@naver.com>
2025-07-16 10:45:22 +0900

    init

---
+++ office/src/main/kotlin/kr/co/finda/office/OfficeApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.finda.office
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OfficeApplication
+
+fun main(args: Array<String>) {
+	runApplication<OfficeApplication>(*args)
+}
---
+++ office/src/test/kotlin/kr/co/finda/office/OfficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.finda.office
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OfficeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
